e7fb5151e75961031907e1bc63c73b4ef7def85c
alexander.zap <alexander.zap@axxessio.com>
2021-02-09 11:36:48 +0100

    Added option to use agent without him learning

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ class Chatbot:
         self.state_tracker = StateTracker(database)
 
         # Create user simulator with list of user goals
-        self.user_simulated = RulebasedUsersim(json.load(open("resources/movie_user_goals.json", "r", encoding="utf-8")))
+        self.user_simulated = RulebasedUsersim(
+            json.load(open("resources/movie_user_goals.json", "r", encoding="utf-8")))
 
         # Create user instance for direct text interactions
         self.user_interactive = User("user/regex_nlu.json")
@@ -35,7 +36,8 @@ class Chatbot:
                               batch_size=256, memory_len=80000, prioritized_memory=True,
                               replay_iter=16, replace_target_iter=200)
 
-    def run(self, n_episodes, step_size=100, success_rate_threshold=0.4, warm_up=False, interactive=False):
+    def run(self, n_episodes, step_size=100, success_rate_threshold=0.4,
+            warm_up=False, interactive=False, learning=True):
         """
         Runs the loop that trains the agent.
 
@@ -69,12 +71,13 @@ class Chatbot:
             prev_agent_action = self.agent.choose_action(prev_observation, warm_up=warm_up)
             while not done:
                 step_counter += 1
-                replay = step_counter % self.agent.replay_iter == 0
                 # print(prev_agent_action)
                 # 2) 3) 4) 5) 6)
                 observation, reward, done, success = self.env_step(prev_agent_action)
-                self.agent.update(prev_observation, prev_agent_action, observation, reward, done,
-                                  warm_up=warm_up, replay=replay)
+                if learning:
+                    replay = step_counter % self.agent.replay_iter == 0
+                    self.agent.update(prev_observation, prev_agent_action, observation, reward, done,
+                                      warm_up=warm_up, replay=replay)
                 # 1) Agent takes action given state tracker's representation of dialogue (observation)
                 agent_action = self.agent.choose_action(observation, warm_up=warm_up)
 
