79c2c096d0142c7dc132189797770916852b97e8
alexander.zap <alexander.zap@axxessio.com>
2021-03-26 16:35:36 +0100

    - Improve GUI for example showcase

--- gui/chat_application.py
+++ gui/chat_application.py
@@ -23,45 +23,64 @@ class ChatApplication:
             beam_width=500, lm_alpha=0.75, lm_beta=1.85)
 
     def _setup_main_window(self):
-        self.window.title("Dialogue Agent")
-        self.window.resizable(width=False, height=False)
-        self.window.configure(width=470, height=600, bg=DARK_BLUE_COLOR)
+        self.window.title("Webpage")
+        self.window.resizable(width=True, height=True)
+        self.window.configure(width=1536, height=864)
+        self.window.bind("<Control-f>", self.toggle_fullscreen)
+        self.window.bind("<Escape>", self.end_fullscreen)
+
+        background_image = tk.PhotoImage(file=r"gui\resources\webpage.png")
+        background_label = tk.Label(master=self.window, image=background_image)
+        background_label.photo = background_image
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        # create nested chat frame
+        nested_frame = tk.Canvas(master=self.window, highlightbackground="black", highlightthickness=1)
+        nested_frame.place(relwidth=0.22, relheight=0.75, relx=0.75, rely=0.2)
+
+        chat_assistant_image_frame = tk.Frame(master=nested_frame)
+        chat_assistant_image_frame.place(relwidth=0.98, relheight=0.33, relx=0.01, rely=0.01)
+        chat_assistant_image = tk.PhotoImage(file=r"gui/resources/chat_assistant.gif")
+        chat_assistant_label = tk.Label(master=chat_assistant_image_frame, image=chat_assistant_image)
+        chat_assistant_label.photo = chat_assistant_image
+        chat_assistant_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        chat_frame = tk.Frame(master=nested_frame, bg=WHITE_COLOR)
+        chat_frame.place(relwidth=0.98, relheight=0.65, relx=0.01, rely=0.34)
 
         # head label
-        head_label = tk.Label(master=self.window, bg=DARK_BLUE_COLOR, fg=WHITE_COLOR, text="Welcome", font=FONT_BOLD,
-                              pady=10)
-        head_label.place(relwidth=1)
-
-        # tiny divider
-        line = tk.Label(master=self.window, width=450, bg=GRAY_COLOR)
-        line.place(relwidth=1, rely=0.07, relheight=0.012)
+        head_label = tk.Label(master=chat_frame, bg=DARK_BLUE_COLOR, fg=WHITE_COLOR, text="Welcome", font=FONT_BOLD)
+        head_label.place(relwidth=1, rely=0.01)
 
         # text widget
-        self.text_widget = tk.Text(master=self.window, width=20, height=2, bg=DARK_BLUE_COLOR, fg=WHITE_COLOR,
+        self.text_widget = tk.Text(master=chat_frame, width=20, height=2, bg=DARK_BLUE_COLOR, fg=WHITE_COLOR,
                                    font=FONT,
-                                   padx=5, pady=5)
-        self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
+                                   padx=5, pady=5, wrap=tk.WORD)
+        self.text_widget.place(relheight=0.745, relwidth=0.96, rely=0.08)
         self.text_widget.configure(cursor="arrow", state=tk.DISABLED)
 
         # scroll bar
-        scrollbar = tk.Scrollbar(master=self.text_widget)
-        scrollbar.place(relheight=1, relx=0.974)
+        scrollbar = tk.Scrollbar(master=chat_frame)
+        scrollbar.place(relheight=0.825, relx=0.95)
         scrollbar.configure(command=self.text_widget.yview)
 
         # bottom label
-        bottom_label = tk.Label(master=self.window, bg=GRAY_COLOR, height=80)
-        bottom_label.place(relwidth=1, rely=0.825)
+        bottom_label = tk.Label(master=chat_frame, bg=GRAY_COLOR)
+        bottom_label.place(relwidth=1, relheight=2.4, rely=0.825)
 
         # message entry box
         self.msg_entry = tk.Entry(bottom_label, bg="#2C3E50", fg=WHITE_COLOR, font=FONT)
-        self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
+        self.msg_entry.place(relwidth=0.98, relheight=0.06, rely=0.006, relx=0.01)
         self.msg_entry.focus()
         self.msg_entry.bind("<Return>", self._on_enter_pressed)
 
-        # send button
-        send_button = tk.Button(master=bottom_label, text="Send", font=FONT_BOLD, width=20, bg=GRAY_COLOR,
-                                command=lambda: self._on_enter_pressed(None))
-        send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
+    def toggle_fullscreen(self, event):
+        self.window.attributes("-fullscreen", True)
+        return "break"
+
+    def end_fullscreen(self, event):
+        self.window.attributes("-fullscreen", False)
+        return "break"
 
     def _on_enter_pressed(self, event):
         msg = self.msg_entry.get()
