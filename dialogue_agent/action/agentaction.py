0c4c579a6e39d0b1adac42cbc0a620a1482db16c
alexander.zap <alexander.zap@axxessio.com>
2021-05-11 16:16:36 +0200

    Moved agent NLG and movie_user_goals.json to application

--- dialogue_agent/action/agentaction.py
+++ dialogue_agent/action/agentaction.py
@@ -1,6 +1,3 @@
-from dialogue_agent.dialog_config import slot_name_translations
-
-
 class AgentAction:
 
     def __init__(self):
@@ -13,30 +10,3 @@ class AgentAction:
 
     def __repr__(self):
         return "{}({!r})".format(self.__class__.__name__, self.__dict__)
-
-    def to_utterance(self):
-        def translate(slot_name):
-            """ Translates slot_name to German by slot_name_translations lookup."""
-            reverted_slot_name_translations = {v: k for k, v in slot_name_translations.items()}
-            if slot_name in reverted_slot_name_translations:
-                slot_name = reverted_slot_name_translations[slot_name]
-            return slot_name
-
-        if self.intent == 'inform' or self.intent == 'match_found':
-            inform_slot_key = translate(list(self.inform_slots.keys())[0])
-            inform_slot_value = list(self.inform_slots.values())[0]
-            if self.intent == 'inform':
-                if inform_slot_value == 'no match available':
-                    return "Ich konnte für {} leider keinen Match finden.".format(inform_slot_key)
-                else:
-                    return "Als {} wäre {} möglich.".format(inform_slot_key, inform_slot_value)
-            elif self.intent == 'match_found':
-                if 'no match available' in inform_slot_value:
-                    return "Ich konnte leider kein passendes Ticket finden."
-                else:
-                    return "Kann ich Ihnen das Ticket {} empfehlen?".format(self.inform_slots)
-        elif self.intent == 'request':
-            request_slot_key = translate(self.request_slots[0])
-            return "Was wünschen Sie als {}?".format(request_slot_key)
-        elif self.intent == 'done':
-            return "Auf Wiedersehen."
